7731d634e4ab6ce4c96fd29694f6c1fe6517db47
sohu-git <zhipinglu@sohu-inc.com>
2013-08-08 14:08:54 +0800

    add math

---
+++ math/1.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: 1.cpp
+    > Author: luzhiping
+    > Mail: isluzp@gmail.com 
+    > Created Time: 2013年07月09日 星期二 09时23分38秒
+ ************************************************************************/
+#include <cstdio>
+#include <math.h>
+
+main(){
+	double i = log(1000)/log(2);
+	printf("%lf\n",i);
+}
